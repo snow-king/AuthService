d0cd98564b22c72ec7ae65b306b207194b9f8245
Гайбадулов Роман Константинович <gaybadulov_rk@irgups.ru>
2023-03-27 10:35:12 +0800

    fix

--- app/models/JWK.go
+++ app/models/JWK.go
@@ -4,7 +4,6 @@ type JWK struct {
 	Keys []struct {
 		Kty string `json:"kty,omitempty"`
 		Kid string `json:"kid,omitempty"`
-		K   string `json:"k,omitempty"`
 		Alg string `json:"alg,omitempty"`
 	} `json:"keys,omitempty"`
 }
--- app/service/JwtInfo.go
+++ app/service/JwtInfo.go
@@ -1,21 +1,18 @@
 package service
 
 import (
-	"AuthService/app/errors"
 	"AuthService/app/models"
-	"encoding/json"
-	"os"
 )
 
 type JWTInfo struct {
 	JWK models.JWK
 }
 
-func (i JWTInfo) InitJWK() (models.JWK, error) {
-	file, err := os.ReadFile("app/assets/symmetric.json")
-	if err != nil {
-		return i.JWK, errors.ErrFileDoesNotExist
-	}
-	err = json.Unmarshal(file, &i.JWK)
-	return i.JWK, nil
+func (i JWTInfo) InitJWK() models.JWK {
+	i.JWK.Keys = append(i.JWK.Keys, struct {
+		Kty string `json:"kty,omitempty"`
+		Kid string `json:"kid,omitempty"`
+		Alg string `json:"alg,omitempty"`
+	}{Kty: "HS", Kid: "NjVBRjY5MDlCMUIwNzU4RTA2QzZFMDQ4QzQ2MDAyQjVDNjk1RTM2Qg", Alg: "HS256"})
+	return i.JWK
 }
