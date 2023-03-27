adf294fb0bd1642a53c251f709915182b8447f13
Гайбадулов Роман Константинович <gaybadulov_rk@irgups.ru>
2023-03-27 14:36:31 +0800

    fix

--- app/service/JwtInfo.go
+++ app/service/JwtInfo.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"AuthService/app/models"
+	"encoding/base64"
 	"github.com/spf13/viper"
 )
 
@@ -10,11 +11,12 @@ type JWTInfo struct {
 }
 
 func (i JWTInfo) InitJWK() models.JWK {
+	key, _ := base64.StdEncoding.DecodeString(viper.GetString("signing_key"))
 	i.JWK.Keys = append(i.JWK.Keys, struct {
 		Kty string `json:"kty,omitempty"`
 		Kid string `json:"kid,omitempty"`
 		K   []byte `json:"k,omitempty"`
 		Alg string `json:"alg,omitempty"`
-	}{Kty: "oct", Kid: viper.GetString("kid"), K: []byte(viper.GetString("signing_key")), Alg: "HS256"})
+	}{Kty: "oct", Kid: viper.GetString("kid"), K: key, Alg: "HS256"})
 	return i.JWK
 }
