cb06b96b5a4de0368ab4f37bb6024fdb543d2d3c
Гайбадулов Роман Константинович <gaybadulov_rk@irgups.ru>
2023-03-27 14:57:10 +0800

    fix

--- app/service/JwtInfo.go
+++ app/service/JwtInfo.go
@@ -11,11 +11,11 @@ type JWTInfo struct {
 }
 
 func (i JWTInfo) InitJWK() models.JWK {
-	key, _ := base64.StdEncoding.DecodeString(viper.GetString("signing_key"))
+	key := base64.StdEncoding.EncodeToString([]byte(viper.GetString("signing_key")))
 	i.JWK.Keys = append(i.JWK.Keys, struct {
 		Kty string `json:"kty,omitempty"`
 		Kid string `json:"kid,omitempty"`
-		K   []byte `json:"k,omitempty"`
+		K   string `json:"k,omitempty"`
 		Alg string `json:"alg,omitempty"`
 	}{Kty: "oct", Kid: viper.GetString("kid"), K: key, Alg: "HS256"})
 	return i.JWK
