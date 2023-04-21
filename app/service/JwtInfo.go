004eb5073f5e8fc207eb234cede22a310ace55ac
Гайбадулов Роман Константинович <gaybadulov_rk@irgups.ru>
2023-04-21 11:02:04 +0800

    fix token generation

--- app/service/JwtInfo.go
+++ app/service/JwtInfo.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"AuthService/app/models"
-	"encoding/base64"
 	"github.com/spf13/viper"
 )
 
@@ -11,12 +10,13 @@ type JWTInfo struct {
 }
 
 func (i JWTInfo) InitJWK() models.JWK {
-	key := base64.StdEncoding.EncodeToString([]byte(viper.GetString("SIGNING_KEY")))
+	key := viper.GetString("SIGNING_KEY")
 	i.JWK.Keys = append(i.JWK.Keys, struct {
 		Kty string `json:"kty,omitempty"`
 		Kid string `json:"kid,omitempty"`
 		K   string `json:"k,omitempty"`
 		Alg string `json:"alg,omitempty"`
+		Use string `json:"use,omitempty"`
 	}{Kty: "oct", Kid: viper.GetString("KID"), K: key, Alg: "HS256"})
 	return i.JWK
 }
